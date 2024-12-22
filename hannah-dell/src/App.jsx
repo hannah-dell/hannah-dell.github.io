6487e45a6a9972db1f21b332fa035928181085db
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-12-22 12:19:10 +0000

    Started home page

--- hannah-dell/src/App.jsx
+++ hannah-dell/src/App.jsx
@@ -1,12 +1,11 @@
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Home />
     </>
   )
 }
