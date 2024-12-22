1d3846158b303f644424d8b7e73a870a23314418
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-12-22 16:06:05 +0000

    Teaching details added

--- hannah-dell/src/pages/Teaching.jsx
+++ hannah-dell/src/pages/Teaching.jsx
@@ -1,5 +1,43 @@
-export default function Teaching() {
+import data from "../data/teaching.json";
+
+export default function Talks() {
+
+  data.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+  const tableRows = data.map(item => {
+    return (
+      <tr key={item.id}>
+        <td>
+          {item.dateText}
+        </td>
+        <td>
+          {item.courseUrl ? (
+            <>
+              <a href={item.courseUrl} target="_blank" rel="noopener noreferrer">{item.name}</a>
+            </>
+          ) : (
+            item.name
+          )}
+        </td>
+        <td>
+          {item.seminarUrl ? (
+            <a href={item.seminarUrl} target="_blank" rel="noopener noreferrer">{item.seminarName}</a>
+          ) : (
+            item.seminarUrl
+          )}
+        </td>
+        <td>
+          {item.location}
+        </td>
+      </tr>
+    )
+  })
+
   return (
-    <>Teach</>
+    <table>
+      <tbody>
+        {tableRows}
+      </tbody>
+    </table>
   )
 }
