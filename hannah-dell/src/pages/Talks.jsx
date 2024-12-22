bc45fb50bb0070e54bbcb0bdcb8c04288e08a590
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-12-22 15:51:21 +0000

    Talks info added

--- hannah-dell/src/pages/Talks.jsx
+++ hannah-dell/src/pages/Talks.jsx
@@ -1,5 +1,41 @@
+import data from "../data/talks.json";
+
 export default function Talks() {
+
+  const tableRows = data.map(item => {
+    return (
+      <tr key={item.id}>
+        <td>
+          {item.notes ? (
+            <>
+              {item.name} <a href={`/notes/${item.notes}`} target="_blank" rel="noopener noreferrer">(notes)</a>
+            </>
+          ) : (
+            item.name
+          )}
+        </td>
+        <td>
+          {item.date}
+        </td>
+        <td>
+          {item.seminarUrl ? (
+            <a href={item.seminarUrl} target="_blank" rel="noopener noreferrer">{item.seminarName}</a>
+          ) : (
+            item.seminarUrl
+          )}
+        </td>
+        <td>
+          {item.location}
+        </td>
+      </tr>
+    )
+  })
+
   return (
-    <>Talks</>
+    <table>
+      <tbody>
+        {tableRows}
+      </tbody>
+    </table>
   )
 }
