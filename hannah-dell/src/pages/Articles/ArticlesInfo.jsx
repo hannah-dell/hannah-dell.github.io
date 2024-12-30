c81371bbef2beeaf13a41dd6ff2152abfeb2bd40
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-12-30 21:19:30 +0000

    Basic article page created

--- hannah-dell/src/pages/Articles/ArticlesInfo.jsx
+++ hannah-dell/src/pages/Articles/ArticlesInfo.jsx
@@ -6,13 +6,17 @@ export default function ArticlesInfo() {
 
   const articles = data.map(item => {
     return (
-      <div key={item.id}>
-        <div>
-          <h3>{item.name}</h3>
-          {item.arxiv && (<a href={item.arxiv} target="_blank" rel="noopener noreferrer">(arXiv)</a>)}
-          {item.pdf && (<a href={`/papers/${item.pdf}`} target="_blank" rel="noopener noreferrer">(PDF)</a>)}
+      <div className="article-container" key={item.id}>
+        <div className="article-info">
+          <h3 className="article-heading">{item.name}</h3>
+          {item.arxiv && (<a className="article-link" href={item.arxiv} target="_blank" rel="noopener noreferrer">(arXiv)</a>)}
+          {item.pdf && (<a className="article-link" href={`/papers/${item.pdf}`} target="_blank" rel="noopener noreferrer">(PDF)</a>)}
         </div>
-        {item.collaborators && <h4>Joint with {item.collaborators}</h4>}
+        {item.collaborators && <h4><em>Joint with {item.collaborators}</em></h4>}
+        <div
+          className="article-abstract"
+          dangerouslySetInnerHTML={{ __html: item.abstract }} // note data must come from safe source
+        />
       </div>
     )
   })
