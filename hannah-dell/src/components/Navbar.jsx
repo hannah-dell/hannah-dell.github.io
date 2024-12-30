cdf02b4a2b2f4dba1890a6cfcc294d064e0a6da5
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-12-30 12:47:33 +0000

    Hamburger menu work

--- hannah-dell/src/components/Navbar.jsx
+++ hannah-dell/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
   const homeName = "Hannah Dell";
 
   const headingNames = (
-    <ul className="hidden md:flex">
+    <ul className="hidden sm:flex">
       {headings.map(heading => (
         <li key={heading.id} className="px-2">
           <Link 
@@ -24,13 +24,25 @@ export default function Navbar() {
 
   return (
     <nav className="w-full bg-navbarBackground shadow-md py-4">
-      <div className="container mx-auto flex items-center">
+      <div className="container mx-auto flex items-center justify-between sm:justify-start">
           <Link
             to="/"
             className="text-lg text-headingYellow no-underline hover:text-hoverYellow transition duration-300 pr-4"
           >
             {homeName}
           </Link>
+
+        <div className="sm:hidden flex items-center">
+          <button 
+            className="text-headingYellow hover:text-hoverYellow transition duration-300"
+            onClick={() => console.log("You clicked me!")}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+        </div>
+
         {headingNames}
       </div>
     </nav>
