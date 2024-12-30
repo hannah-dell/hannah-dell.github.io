244c6c521cc44e296c810b33a6ccc5b2c82e8e9e
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-12-30 12:37:35 +0000

    Fixed important css

--- hannah-dell/src/components/Navbar.jsx
+++ hannah-dell/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
         <li key={heading.id} className="px-2">
           <Link 
             to={`${heading.name.toLowerCase()}`}
-            className="text-headingYellow hover:text-hoverYellow transition duration-300"
+            className="text-headingYellow no-underline hover:text-hoverYellow transition duration-300"
           >
             {heading.name}
           </Link>
@@ -25,11 +25,12 @@ export default function Navbar() {
   return (
     <nav className="w-full bg-navbarBackground shadow-md py-4">
       <div className="container mx-auto flex items-center">
-        <div className="text-lg text-headingYellow hover:text-hoverYellow transition duration-300 pr-4">
-          <Link to="/">
+          <Link
+            to="/"
+            className="text-lg text-headingYellow no-underline hover:text-hoverYellow transition duration-300 pr-4"
+          >
             {homeName}
           </Link>
-        </div>
         {headingNames}
       </div>
     </nav>
--- hannah-dell/src/pages/Home.jsx
+++ hannah-dell/src/pages/Home.jsx
@@ -11,11 +11,11 @@ export default function Home() {
               alt="Hannah Dell" 
               className="img-me"  
             />
-            
-            I am a postdoc in the <a className="link" href="https://www.math.uni-bonn.de/">Mathematical Institute at the University of Bonn</a>, in the working group of <a className="link" href="https://www.math.uni-bonn.de/~huybrech/">Daniel Huybrechts</a>. I'm funded by the ERC synergy grant <a className="link" href="https://www.erc-hyperk.org/">HyperK</a>.
+
+            I am a postdoc in the <a href="https://www.math.uni-bonn.de/">Mathematical Institute at the University of Bonn</a>, in the working group of <a href="https://www.math.uni-bonn.de/~huybrech/">Daniel Huybrechts</a>. I'm funded by the ERC synergy grant <a href="https://www.erc-hyperk.org/">HyperK</a>.
           </p>
 
-          <p>I did my PhD at the University of Edinburgh under the supervision of <a className="link" href="https://www.maths.ed.ac.uk/~abayer/">Arend Bayer</a>.</p>
+          <p>I did my PhD at the University of Edinburgh under the supervision of <a href="https://www.maths.ed.ac.uk/~abayer/">Arend Bayer</a>.</p>
 
           <p><b>Email:</b> hdell (at) math (dot) uni-bonn (dot) de</p>
 
@@ -30,24 +30,24 @@ export default function Home() {
           <p>In my first project, I studied the stability manifold of (smooth projective) surfaces which are free abelian quotients. One approach is via group actions on triangulated categories, another is via Le Potier functions:</p>
 
           <ul className="bullet">
-            <li><em>Stability Conditions on Free Abelian Quotients</em>, <a className="link" href="https://arxiv.org/abs/2307.00815">arXiv</a>.</li>
+            <li><em>Stability Conditions on Free Abelian Quotients</em>, <a href="https://arxiv.org/abs/2307.00815">arXiv</a>.</li>
           </ul>
 
           <p>I am also thinking about derived categories of cyclic covers, and the question of whether such a cover can be recovered (up to isomorphism) from its Kuznetsov component:</p>
 
           <ul className="bullet">
-            <li><em>Cyclic covers: Hodge theory and categorical Torelli theorems</em>, joint with Augustinas Jacovskis and Franco Rota, <a className="link" href="https://arxiv.org/abs/2310.13651">arXiv</a>.</li>
+            <li><em>Cyclic covers: Hodge theory and categorical Torelli theorems</em>, joint with Augustinas Jacovskis and Franco Rota, <a href="https://arxiv.org/abs/2310.13651">arXiv</a>.</li>
           </ul>
 
           <p>I have also been investigating how stability conditions behave under actions of fusion categories (=<em>tensor categories with extra structure</em>). A crucial example is that, if a finite group G acts on a category D, then the corresponding G-equivariant category is a module category over Rep(G) (which is a fusion category). As an application, we generalise a result from my first project to non abelian groups.</p>
 
           <ul className="bullet">
-            <li><em>Fusion-equivariant stability conditions and Morita duality</em>, joint with Edmund Heng and Anthony Licata, <a className="link" href="https://arxiv.org/abs/2311.06857">arXiv</a>.</li>
+            <li><em>Fusion-equivariant stability conditions and Morita duality</em>, joint with Edmund Heng and Anthony Licata, <a href="https://arxiv.org/abs/2311.06857">arXiv</a>.</li>
           </ul>
 
           <h3>Other Activities</h3>
 
-          <p>I previously organised GAeL XXX (University of Warwick, July 2023), GAeL XXIX (Université Paris-Saclay, May 2022), and the Edinburgh Geometry seminar (EDGE).</p>
+          <p>I previously organised <a href="https://sites.google.com/view/gaelxxx/home">GAeL XXX</a> (University of Warwick, July 2023), <a href="https://sites.google.com/view/gaelxxix/home">GAeL XXIX</a> (Université Paris-Saclay, May 2022), and the Edinburgh Geometry seminar (<a href="https://hodge.maths.ed.ac.uk/?page_id=36">EDGE</a>).</p>
 
           <p>When I'm not doing maths, I like to hike, climb and kayak.</p>
 
