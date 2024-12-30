700e04929f4c4fc7eaafb74ade265ea463e41460
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-12-30 18:47:43 +0000

    Removed bullet class

--- hannah-dell/src/pages/Home.jsx
+++ hannah-dell/src/pages/Home.jsx
@@ -29,19 +29,19 @@ export default function Home() {
 
           <p>In my first project, I studied the stability manifold of (smooth projective) surfaces which are free abelian quotients. One approach is via group actions on triangulated categories, another is via Le Potier functions:</p>
 
-          <ul className="bullet">
+          <ul>
             <li><em>Stability Conditions on Free Abelian Quotients</em>, <a href="https://arxiv.org/abs/2307.00815">arXiv</a>.</li>
           </ul>
 
           <p>I am also thinking about derived categories of cyclic covers, and the question of whether such a cover can be recovered (up to isomorphism) from its Kuznetsov component:</p>
 
-          <ul className="bullet">
+          <ul>
             <li><em>Cyclic covers: Hodge theory and categorical Torelli theorems</em>, joint with Augustinas Jacovskis and Franco Rota, <a href="https://arxiv.org/abs/2310.13651">arXiv</a>.</li>
           </ul>
 
           <p>I have also been investigating how stability conditions behave under actions of fusion categories (=<em>tensor categories with extra structure</em>). A crucial example is that, if a finite group G acts on a category D, then the corresponding G-equivariant category is a module category over Rep(G) (which is a fusion category). As an application, we generalise a result from my first project to non abelian groups.</p>
 
-          <ul className="bullet">
+          <ul>
             <li><em>Fusion-equivariant stability conditions and Morita duality</em>, joint with Edmund Heng and Anthony Licata, <a href="https://arxiv.org/abs/2311.06857">arXiv</a>.</li>
           </ul>
 
