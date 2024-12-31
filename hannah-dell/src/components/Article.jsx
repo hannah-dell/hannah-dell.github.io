a2523356eadd1da0807fb75d19f851bbfd85bcca
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-12-31 18:57:31 +0000

    Only one accordion open at a time

--- hannah-dell/src/components/Article.jsx
+++ hannah-dell/src/components/Article.jsx
@@ -1,42 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { MathJax, MathJaxContext } from 'better-react-mathjax';
 
 
-export default function Article({ item, isCollapsible }) {
+export default function Article(props) {
     
-  const [isOpen, setIsOpen] = useState(isCollapsible ? false : true);
-
   // Rerender MathJax on change
   useEffect(() => {
-    if (isOpen && window.MathJax) {
+    if (props.isOpen && window.MathJax) {
       window.MathJax.typeset();
     }
-  }, [isOpen, item.abstract]);
+  }, [props.isOpen, props.item.abstract]);
 
   return (
-    <div className="article-container" key={item.id}>
+    <div className="article-container" key={props.item.id}>
         <div className="article-top">
           <div className="article-info">
-            {isCollapsible && <button className="article-toggle" onClick={() => setIsOpen(!isOpen)}>{isOpen ? "-" : "+"}</button>}
-            <h3 className="article-heading">{item.name}</h3>
-            {item.showYear && <span className="article-detail">({new Date(item.date).toLocaleString("en-GB", { year: "numeric" })})</span>}
-            {item.arxiv && (<a className="article-link" href={item.arxiv} target="_blank" rel="noopener noreferrer">(arXiv)</a>)}
-            {item.pdf && (<a className="article-link" href={`/papers/${item.pdf}`} target="_blank" rel="noopener noreferrer">(PDF)</a>)}
+            {props.isCollapsible && <button className="article-toggle" onClick={props.handleClick}>{props.isOpen ? "-" : "+"}</button>}
+            <h3 className="article-heading">{props.item.name}</h3>
+            {props.item.showYear && <span className="article-detail">({new Date(props.item.date).toLocaleString("en-GB", { year: "numeric" })})</span>}
+            {props.item.arxiv && (<a className="article-link" href={props.item.arxiv} target="_blank" rel="noopener noreferrer">(arXiv)</a>)}
+            {props.item.pdf && (<a className="article-link" href={`/papers/${props.item.pdf}`} target="_blank" rel="noopener noreferrer">(PDF)</a>)}
           </div>
           <div className="article-journal">
-            {item.journalStatus && 
-              item.journalUrl 
-                ? <h5><em><a className="article-link" href={item.journalUrl} target="_blank" rel="noopener noreferrer">{item.journalStatus}</a></em></h5> 
-                : <h5><em>{item.journalStatus}</em></h5>
+            {props.item.journalStatus && 
+              props.item.journalUrl 
+                ? <h5><em><a className="article-link" href={props.item.journalUrl} target="_blank" rel="noopener noreferrer">{props.item.journalStatus}</a></em></h5> 
+                : <h5><em>{props.item.journalStatus}</em></h5>
             }
           </div>
         </div>
-        {item.collaborators && <h4><em>Joint with {item.collaborators}</em></h4>}
-        {isOpen &&
+        {props.item.collaborators && <h4><em>Joint with {props.item.collaborators}</em></h4>}
+        {props.isOpen &&
           <MathJaxContext>
             <div
               className="article-abstract"
-              dangerouslySetInnerHTML={{ __html: item.abstract }} // note data must come from safe source
+              dangerouslySetInnerHTML={{ __html: props.item.abstract }} // note data must come from safe source
             />
           </MathJaxContext>
         }
--- hannah-dell/src/components/Articles.jsx
+++ hannah-dell/src/components/Articles.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+
 import data from '../data/research.json';
 import Article from '../components/Article';
 
 export default function Articles(props) {
   data.sort((a, b) => new Date(b.date) - new Date(a.date));
   const newData = props.pinnedOnly ? data.filter(item => item.pinned) : data;
+
+  const [expandedArticleId, setExpandedArticleId] = useState(null);
+
+  const handleToggle = (id) => {
+    setExpandedArticleId((prev) => (prev === id ? null : id));
+  }
 
   return (
     <>
@@ -12,7 +20,13 @@ export default function Articles(props) {
           <h2>{props.heading ? props.heading : "Articles"}</h2>
           {newData.map(item => {
             return (
-              <Article item={item} key={item.id} isCollapsible={props.isCollapsible}/>
+              <Article 
+                item={item}
+                key={item.id}
+                isCollapsible={props.isCollapsible}
+                isOpen={props.isCollapsible ? item.id === expandedArticleId : true}
+                handleClick={() => handleToggle(item.id)}
+              />
             )
           })}
         </div>
